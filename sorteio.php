80e9a50af2520f9e892e151603d57bb53ff23cc2
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-05-14 20:45:04 -0300

    Commit inicial

---
+++ sorteio.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Sorteio</title>
+</head>
+<body>
+<div class="container">
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Primeiro</th>
+      <th scope="col">Último</th>
+      <th scope="col">Nickname</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<?php
+$facilidade = include 'facilidade.php';
+$dificuldade = include 'dificuldade.php';
+echo '<pre/>';
+$quantifacilidade = 0;
+foreach($facilidade  as $fa){
+    $quantifacilidade+=$fa['alunos'];
+}
+echo $quantifacilidade;
+echo '<br/>';
+$contdif = 0;
+foreach($dificuldade  as $fa){
+    $contdif+=$fa['tutores'];
+}
+echo $contdif;
+
+echo "<br>SEGUNDA PARTE: SORTEIO";
+
+foreach($dificuldade  as $fa){
+    echo "<br>".$fa['nome'].": ".$fa['tutores'];
+}
+echo "<br>";
+echo "Com dificuldade: ".count($dificuldade);
+echo "<br>";
+echo "Com facilidade: ".count($facilidade);
+foreach ($dificuldade as $value) {
+    $value['tutores']>1?print_r($value):0;
+}
+?>
+<!-- Conteúdo aqui -->
+</div>
+</body>
+</html>
